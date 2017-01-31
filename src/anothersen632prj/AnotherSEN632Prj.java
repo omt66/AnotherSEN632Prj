43bd11fa584bf8104b086412491b1c73c4396013
Ogun @ Win10 <otigli@gmail.com>
2017-01-30 19:25:20 -0800

    Initial commit

---
+++ src/anothersen632prj/AnotherSEN632Prj.java
@@ -0,0 +1,25 @@
+/*
+ * OT: To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anothersen632prj;
+
+/**
+ *
+ * @author Ogun otigli@gmail.com
+ */
+public class AnotherSEN632Prj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        println("--- Just a Demo ---");
+    }
+
+    private static void println(String msg) {
+        System.out.println(msg);
+    }
+    
+}
